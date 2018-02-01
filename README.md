3a844f372844831a633f39b4d4622b4b4ae943d4
KleinYuan <kaiwen.yuan1992@gmail.com>
2018-02-01 13:58:59 -0800

    Working script

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# tf-segmentation
+# Introduction
+
+Real time segmentation inference production ready code based on DeepLab.
+
+# Dependencies
+
+- [X] Python 2.X
+
+- [X] Tensorflow > 1.0.0
+
+- [X] OpenCV
+
+* No GPU required
+
+# Run Demo
+
+```
+python inference.py
+```
--- inference.py
+++ inference.py
@@ -1,9 +1,5 @@
 from __future__ import print_function
 
-import argparse
-import os
-import sys
-import time
 import scipy.io as sio
 from PIL import Image
 import cv2
@@ -12,30 +8,23 @@ import numpy as np
 
 from model import DeepLabResNetModel
 
-IMG_MEAN = np.array((104.00698793,116.66876762,122.67891434), dtype=np.float32)
+IMG_MEAN = np.array((104.00698793, 116.66876762, 122.67891434), dtype=np.float32)
 
 NUM_CLASSES = 27
-SAVE_DIR = './output/'
-RESTORE_PATH = ''
-matfn = 'color150.mat'
+COLOR_MAT_FP = 'color150.mat'
 
-def get_arguments():
-    parser = argparse.ArgumentParser(description="Indoor segmentation parser.")
-    parser.add_argument("--restore_from", type=str, default=RESTORE_PATH,
-                        help="checkpoint location")
 
-    return parser.parse_args()
-
-def read_labelcolours(matfn):
-    mat = sio.loadmat(matfn)
+def decode_label_colours(mat_fp):
+    mat = sio.loadmat(mat_fp)
     color_table = mat['colors']
     shape = color_table.shape
     color_list = [tuple(color_table[i]) for i in range(shape[0])]
 
     return color_list
 
+
 def decode_labels(mask, num_images=1, num_classes=150):
-    label_colours = read_labelcolours(matfn)
+    label_colours = decode_label_colours(COLOR_MAT_FP)
 
     n, h, w, c = mask.shape
     assert(n >= num_images), 'Batch size %d should be greater or equal than number of images to save %d.' % (n, num_images)
@@ -50,73 +39,82 @@ def decode_labels(mask, num_images=1, num_classes=150):
       outputs[i] = np.array(img)
     return outputs
 
-def load(saver, sess, ckpt_path):
-    saver.restore(sess, ckpt_path)
-    print("Restored model parameters from {}".format(ckpt_path))
 
 
-class Tester(object):
+class App(object):
     def __init__(self):
-        self.name = 'Tester'
+        self.name = 'App'
+        self.model_path = './model/'
         self.session = None
         self.loader = None
         self.ckpt = None
         self.net = None
+        self.img_tf = None
+        self.cap = None
         self.in_progress = False
+        self.img_shape = 300
+        self.input_feed_shape = (1, self.img_shape, self.img_shape, 3)
+
+    def _tf_init(self):
+        self.img_tf = tf.placeholder(dtype=tf.float32, shape=self.input_feed_shape)
+        self.net = DeepLabResNetModel({'data': self.img_tf}, is_training=False, num_classes=NUM_CLASSES)
+        self.config = tf.ConfigProto()
+        self.config.gpu_options.allow_growth = True
+
+    def _pre_process(self, frame):
+        img_resized = cv2.resize(frame.copy(), (self.img_shape, self.img_shape))
+        img_feed = np.array(img_resized, dtype=float)
+        img_feed = np.expand_dims(img_feed, axis=0)
+        return img_resized, img_feed
+
+    def _load_trained_model(self, saver, sess, ckpt_path):
+        saver.restore(sess, ckpt_path)
+        print("Restored model parameters from {}".format(ckpt_path))
 
     def run(self):
-        args = get_arguments()
+        self._tf_init()
 
-        cap = cv2.VideoCapture(0)
+        self.cap = cv2.VideoCapture(0)
         while True:
-            ret, frame = cap.read()
-            img = cv2.resize(frame.copy(), (300, 300))
+            ret, frame = self.cap.read()
             if ret and (not self.in_progress):
-
-                img = tf.cast(img, dtype=tf.float32)
-                self.net = DeepLabResNetModel({'data': tf.expand_dims(img, dim=0)}, is_training=False, num_classes=NUM_CLASSES)
+                img_resized, img_feed = self._pre_process(frame)
                 restore_var = tf.global_variables()
-                # Predictions.
                 raw_output = self.net.layers['fc_out']
-                raw_output_up = tf.image.resize_bilinear(raw_output, tf.shape(img)[0:2, ])
+                raw_output_up = tf.image.resize_bilinear(raw_output, tf.shape(img_resized)[0:2, ])
                 raw_output_up = tf.argmax(raw_output_up, dimension=3)
                 pred = tf.expand_dims(raw_output_up, dim=3)
 
-                # Set up TF session and initialize variables.
-                config = tf.ConfigProto()
-                config.gpu_options.allow_growth = True
                 if not self.session:
-                    self.session = tf.Session(config=config)
+                    self.session = tf.Session(config=self.config)
                     init = tf.global_variables_initializer()
-
                     self.session.run(init)
-                # Load weights.
 
+                # Load pre-trained model
                 if self.ckpt is None:
-                    self.ckpt = tf.train.get_checkpoint_state(args.restore_from)
+                    self.ckpt = tf.train.get_checkpoint_state(self.model_path)
                     if self.ckpt and self.ckpt.model_checkpoint_path and (self.loader is None):
                         self.loader = tf.train.Saver(var_list=restore_var)
-                        load(self.loader, self.session, self.ckpt.model_checkpoint_path)
+                        self._load_trained_model(self.loader, self.session, self.ckpt.model_checkpoint_path)
                     else:
                         print('No checkpoint file found. or loaded!')
-                print('[In Progress] Predicting')
+
                 self.in_progress = True
-                preds = self.session.run(pred)
+                _ops = self.session.run(pred, feed_dict={self.img_tf: img_feed})
                 self.in_progress = False
-                print('[Done] Prediction done!')
-                msk = decode_labels(preds, num_classes=NUM_CLASSES)
+                msk = decode_labels(_ops, num_classes=NUM_CLASSES)
                 cv2.imshow('mask', msk[0])
-            else:
-                print('Drop frame = %s | In Progress = %s' % (not ret, self.in_progress))
+
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
-        cap.release()
+
+        self.cap.release()
         cv2.destroyAllWindows()
 
 
 def main():
-    test = Tester()
-    test.run()
+    app = App()
+    app.run()
 
 
 if __name__ == '__main__':
