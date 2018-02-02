95aea72b1e1665e089e22f0f876a9bc698de7901
KleinYuan <kaiwen.yuan1992@gmail.com>
2018-02-01 17:08:43 -0800

    Disable frozen pb

--- inference.py
+++ inference.py
@@ -14,31 +14,6 @@ NUM_CLASSES = 27
 COLOR_MAT_FP = 'color150.mat'
 
 
-def decode_label_colours(mat_fp):
-    mat = sio.loadmat(mat_fp)
-    color_table = mat['colors']
-    shape = color_table.shape
-    color_list = [tuple(color_table[i]) for i in range(shape[0])]
-
-    return color_list
-
-
-def decode_labels(mask, num_images=1, num_classes=150):
-    label_colours = decode_label_colours(COLOR_MAT_FP)
-
-    n, h, w, c = mask.shape
-    assert(n >= num_images), 'Batch size %d should be greater or equal than number of images to save %d.' % (n, num_images)
-    outputs = np.zeros((num_images, h, w, 3), dtype=np.uint8)
-    for i in range(num_images):
-      img = Image.new('RGB', (len(mask[i, 0]), len(mask[i])))
-      pixels = img.load()
-      for j_, j in enumerate(mask[i, :, :, 0]):
-          for k_, k in enumerate(j):
-              if k < num_classes:
-                  pixels[k_,j_] = label_colours[k]
-      outputs[i] = np.array(img)
-    return outputs
-
 
 class App(object):
     def __init__(self):
@@ -46,7 +21,7 @@ class App(object):
         self.model_path = './model/'
         self.graph_fp = './model/resnet101.pb'
         self.session = None
-        self.loader = None
+        self.saver = None
         self.ckpt = None
         self.net = None
         self.img_tf = None
@@ -56,11 +31,48 @@ class App(object):
         self.img_shape = 300
         self.input_feed_shape = (1, self.img_shape, self.img_shape, 3)
 
+    @staticmethod
+    def decode_label_colours(mat_fp):
+        mat = sio.loadmat(mat_fp)
+        color_table = mat['colors']
+        shape = color_table.shape
+        color_list = [tuple(color_table[i]) for i in range(shape[0])]
+
+        return color_list
+
+    def decode_labels(self, mask, num_images=1, num_classes=150):
+        label_colours = self.decode_label_colours(COLOR_MAT_FP)
+        n, h, w, c = mask.shape
+        assert (n >= num_images), 'Batch size %d should be greater or equal than number of images to save %d.' % (
+        n, num_images)
+        outputs = np.zeros((num_images, h, w, 3), dtype=np.uint8)
+        for i in range(num_images):
+            img = Image.new('RGB', (len(mask[i, 0]), len(mask[i])))
+            pixels = img.load()
+            for j_, j in enumerate(mask[i, :, :, 0]):
+                for k_, k in enumerate(j):
+                    if k < num_classes:
+                        pixels[k_, j_] = label_colours[k]
+            outputs[i] = np.array(img)
+        return outputs
+
     def _tf_init(self):
         self.img_tf = tf.placeholder(dtype=tf.float32, shape=self.input_feed_shape)
         self.net = DeepLabResNetModel({'data': self.img_tf}, is_training=False, num_classes=NUM_CLASSES)
         self.config = tf.ConfigProto()
         self.config.gpu_options.allow_growth = True
+        self.session = tf.Session(config=self.config)
+        init = tf.global_variables_initializer()
+        self.session.run(init)
+
+        # Load pre-trained model
+        self.ckpt = tf.train.get_checkpoint_state(self.model_path)
+        if self.ckpt and self.ckpt.model_checkpoint_path and (self.saver is None):
+            self.saver = tf.train.Saver(var_list=tf.global_variables())
+            self.saver.restore(self.session, self.ckpt.model_checkpoint_path)
+            print("Restored model parameters from {}".format(self.ckpt.model_checkpoint_path))
+        else:
+            print('No checkpoint file found. or loaded!')
 
     def _pre_process(self, frame):
         print('Pre-processing image ...')
@@ -69,10 +81,6 @@ class App(object):
         img_feed = np.expand_dims(img_feed, axis=0)
         print('Pre-processed image!')
         return img_resized, img_feed
-
-    def _load_trained_model(self, saver, sess, ckpt_path):
-        saver.restore(sess, ckpt_path)
-        print("Restored model parameters from {}".format(ckpt_path))
 
     def _load_graph(self):
         print('Loading graph from %s ...' % self.graph_fp)
@@ -108,16 +116,17 @@ class App(object):
             raw_output_up = tf.argmax(raw_output_up, dimension=3)
             pred_ops = tf.expand_dims(raw_output_up, dim=3)
 
-            prediction = self.session.run([pred_ops], feed_dict={self.input_data_tensor: img_feed})
+            prediction = self.session.run(pred_ops, feed_dict={self.input_data_tensor: img_feed})
             print('Prediction Done!')
 
             self.prediction = prediction[0]
-            msk = decode_labels(self.prediction, num_classes=NUM_CLASSES)
+            msk = self.decode_labels(self.prediction, num_classes=NUM_CLASSES)
         self.in_progress = False
         print('Prediction Done !')
         return msk
 
-    def run(self):
+    # Debugging
+    def run_with_frozen_pb(self):
         self._load_graph()
         self._init_predictor()
 
@@ -134,49 +143,30 @@ class App(object):
         self.cap.release()
         cv2.destroyAllWindows()
 
-    # def run_from_model(self):
-    #     self._tf_init()
-    #
-    #     self.cap = cv2.VideoCapture(0)
-    #     while True:
-    #         ret, frame = self.cap.read()
-    #         if ret and (not self.in_progress):
-    #             img_resized, img_feed = self._pre_process(frame)
-    #             restore_var = tf.global_variables()
-    #             raw_output = self.net.layers['fc_out']
-    #             print('raw_output: ', raw_output)
-    #             raw_output_up = tf.image.resize_bilinear(raw_output, tf.shape(img_resized)[0:2, ])
-    #             raw_output_up = tf.argmax(raw_output_up, dimension=3)
-    #             pred = tf.expand_dims(raw_output_up, dim=3)
-    #
-    #
-    #             if not self.session:
-    #                 self.session = tf.Session(config=self.config)
-    #                 init = tf.global_variables_initializer()
-    #                 self.session.run(init)
-    #
-    #             # Load pre-trained model
-    #             if self.ckpt is None:
-    #                 self.ckpt = tf.train.get_checkpoint_state(self.model_path)
-    #                 if self.ckpt and self.ckpt.model_checkpoint_path and (self.loader is None):
-    #                     self.loader = tf.train.Saver(var_list=restore_var)
-    #                     self._load_trained_model(self.loader, self.session, self.ckpt.model_checkpoint_path)
-    #                 else:
-    #                     print('No checkpoint file found. or loaded!')
-    #
-    #             self.in_progress = True
-    #             print('self.img_tf: ', self.img_tf)
-    #             print('pred: ', pred)
-    #             _ops = self.session.run(pred, feed_dict={self.img_tf: img_feed})
-    #             self.in_progress = False
-    #             msk = decode_labels(_ops, num_classes=NUM_CLASSES)
-    #             cv2.imshow('mask', msk[0])
-    #
-    #         if cv2.waitKey(1) & 0xFF == ord('q'):
-    #             break
-    #
-    #     self.cap.release()
-    #     cv2.destroyAllWindows()
+    def run(self):
+        self._tf_init()
+        self.cap = cv2.VideoCapture(0)
+        while True:
+            ret, frame = self.cap.read()
+            if ret and (not self.in_progress):
+                img_resized, img_feed = self._pre_process(frame)
+
+                raw_output = self.net.layers['fc_out']
+                raw_output_up = tf.image.resize_bilinear(raw_output, tf.shape(img_resized)[0:2, ])
+                raw_output_up = tf.argmax(raw_output_up, dimension=3)
+                pred = tf.expand_dims(raw_output_up, dim=3)
+
+                self.in_progress = True
+                _ops = self.session.run(pred, feed_dict={self.img_tf: img_feed})
+                self.in_progress = False
+                msk = self.decode_labels(_ops, num_classes=NUM_CLASSES)
+                cv2.imshow('mask', msk[0])
+
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+
+        self.cap.release()
+        cv2.destroyAllWindows()
 
 
 def main():
